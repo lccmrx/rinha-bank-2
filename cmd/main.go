896806e1534d40f7efbdbe87d35ab757dad2481c
Lucca Marques <devlucca@gmail.com>
2024-03-01 01:08:16 -0300

    feat: removes some unecessary function returns

--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,15 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 var (
 	ctx       context.Context
-	err       error
 	db        *pgxpool.Pool
 	clientMem = make(map[string]*client)
 )
@@ -61,10 +60,12 @@ func init() {
 		"5432",             // port
 	)
 
-	db, err = pgxpool.New(ctx, connStr)
+	dbInstance, err := pgxpool.New(ctx, connStr)
 	if err != nil {
 		panic(err)
 	}
+
+	db = dbInstance
 
 	rows, _ := db.Query(ctx, "select id, \"limit\" from clients")
 	for rows.Next() {
@@ -76,107 +77,105 @@ func init() {
 
 func main() {
 	r := http.NewServeMux()
-	r.Handle("GET /clientes/{id}/extrato", getStatment())
-	r.Handle("POST /clientes/{id}/transacoes", saveTransaction())
+	r.Handle("GET /clientes/{id}/extrato", http.HandlerFunc(getStatment))
+	r.Handle("POST /clientes/{id}/transacoes", http.HandlerFunc(saveTransaction))
 
 	http.ListenAndServe(":9999", r)
 }
 
-func getStatment() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		id := r.PathValue("id")
-		if id < "1" || id > "5" {
-			w.WriteHeader(404)
-			return
-		}
+func getStatment(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	id := r.PathValue("id")
+	if id < "1" || id > "5" {
+		w.WriteHeader(404)
+		return
+	}
 
-		client := clientMem[id]
-		db.QueryRow(ctx, "SELECT balance FROM clients WHERE id = $1", id).Scan(&client.Balance)
+	client := clientMem[id]
+	db.QueryRow(ctx, "SELECT balance FROM clients WHERE id = $1", id).Scan(&client.Balance)
 
-		rows, _ := db.Query(ctx, "SELECT type, value, description, timestamp FROM transactions WHERE client_id = $1 order by timestamp desc limit 10", id)
-		defer rows.Close()
+	rows, _ := db.Query(ctx, "SELECT type, value, description, timestamp FROM transactions WHERE client_id = $1 order by timestamp desc limit 10", id)
+	defer rows.Close()
 
-		statementTxs := make([]statementTransactions, 0)
-		for rows.Next() {
-			var transaction transaction
-			err = rows.Scan(
-				&transaction.Type,
-				&transaction.Value,
-				&transaction.Description,
-				&transaction.Timestamp,
-			)
+	statementTxs := make([]statementTransactions, 0)
+	for rows.Next() {
+		var transaction transaction
+		rows.Scan(
+			&transaction.Type,
+			&transaction.Value,
+			&transaction.Description,
+			&transaction.Timestamp,
+		)
 
-			statementTxs = append(statementTxs, statementTransactions{
-				Value:       transaction.Value,
-				Type:        transaction.Type,
-				Description: transaction.Description,
-				Timestamp:   transaction.Timestamp.Format(time.RFC3339Nano),
-			})
-		}
+		statementTxs = append(statementTxs, statementTransactions{
+			Value:       transaction.Value,
+			Type:        transaction.Type,
+			Description: transaction.Description,
+			Timestamp:   transaction.Timestamp.Format(time.RFC3339Nano),
+		})
+	}
 
-		statement := statement{
-			Balance: statementBalance{
-				Total:         client.Balance,
-				Limit:         client.Limit,
-				StatementDate: time.Now(),
-			},
-			LastTransactions: statementTxs,
-		}
+	statement := statement{
+		Balance: statementBalance{
+			Total:         client.Balance,
+			Limit:         client.Limit,
+			StatementDate: time.Now(),
+		},
+		LastTransactions: statementTxs,
+	}
 
-		data, _ := json.Marshal(statement)
+	data, _ := json.Marshal(statement)
 
-		w.WriteHeader(200)
-		w.Write(data)
-	})
+	w.WriteHeader(200)
+	w.Write(data)
 }
 
-func saveTransaction() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		id := r.PathValue("id")
+func saveTransaction(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
+	ctx := r.Context()
+	rID := uuid.NewString()
+	id := r.PathValue("id")
 
-		if id < "1" || id > "5" {
-			w.WriteHeader(404)
-			return
-		}
+	if id < "1" || id > "5" {
+		w.WriteHeader(404)
+		return
+	}
 
-		var transaction *transaction
-		bodyBytes, _ := io.ReadAll(r.Body)
-		err = json.Unmarshal(bodyBytes, &transaction)
-		if err != nil {
-			w.WriteHeader(422)
-			return
-		}
+	compareTime := time.Now()
+	var transaction *transaction
+	json.NewDecoder(r.Body).Decode(&transaction)
+	fmt.Printf("[%s] Time took to decode JSON: %v\n", rID, time.Since(compareTime).Microseconds())
 
-		if transaction.Type != "d" && transaction.Type != "c" {
-			w.WriteHeader(422)
-			return
-		}
+	if transaction.Type != "d" && transaction.Type != "c" {
+		w.WriteHeader(422)
+		return
+	}
 
-		if transaction.Value == 0 {
-			w.WriteHeader(422)
-			return
-		}
+	if transaction.Value == 0 {
+		w.WriteHeader(422)
+		return
+	}
 
-		descLen := len(transaction.Description)
-		if descLen < 1 || descLen > 10 {
-			w.WriteHeader(422)
-			return
-		}
+	descLen := len(transaction.Description)
+	if descLen < 1 || descLen > 10 {
+		w.WriteHeader(422)
+		return
+	}
 
-		var balance *int
-		db.QueryRow(ctx, "call transact($1, $2, $3, $4)", id, transaction.Value, transaction.Type, transaction.Description).Scan(&balance)
-		if balance == nil {
-			w.WriteHeader(422)
-			return
-		}
+	compareTime = time.Now()
+	var balance *int
+	db.QueryRow(ctx, "call transact($1, $2, $3, $4)", id, transaction.Value, transaction.Type, transaction.Description).Scan(&balance)
+	fmt.Printf("[%s] Time took to call procedure: %v\n", rID, time.Since(compareTime).Microseconds())
+	if balance == nil {
+		w.WriteHeader(422)
+		return
+	}
 
-		client := clientMem[id]
-		client.Balance = *balance
-		data, _ := json.Marshal(client)
+	client := clientMem[id]
+	client.Balance = *balance
+	data, _ := json.Marshal(client)
 
-		w.WriteHeader(200)
-		w.Write(data)
-	})
+	w.WriteHeader(200)
+	w.Write(data)
+	fmt.Printf("[%s] Time took to process request: %v\n", rID, time.Since(startTime).Microseconds())
 }
